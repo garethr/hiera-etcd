5137ea36f5cba5c5c27904a1dee88c33fd2ecdf6
Giuseppe Lavagetto <lavagetto@gmail.com>
2013-09-26 16:59:26 +0200

    Moved from http client implementation to etcd-rb implementation.
    We use the https://github.com/iconara/etcd-rb code; also, I added
    a support for hashes and arrays in hiera fetching data stored in json
    format in etcd.

--- lib/hiera/backend/etcd_backend.rb
+++ lib/hiera/backend/etcd_backend.rb
@@ -4,13 +4,12 @@ class Hiera
     class Etcd_backend
 
       def initialize
-        require 'net/http'
-        require 'net/https'
+        require 'etcd'
         require 'json'
         @config = Config[:http]
-        @http = Net::HTTP.new(@config[:host], @config[:port])
-        @http.read_timeout = @config[:http_read_timeout] || 10
-        @http.open_timeout = @config[:http_connect_timeout] || 10
+        etcd_uri = "#{@config[:host]}:#{@config[:port]}"
+        @client = Etcd::Client.new(uri: etcd_uri)
+        @client.connect
       end
 
       def lookup(key, scope, order_override, resolution_type)
@@ -22,48 +21,40 @@ class Hiera
         paths.insert(0, order_override) if order_override
 
         paths.each do |path|
-          Hiera.debug("[hiera-etcd]: Lookup #{@config[:host]}:#{@config[:port]}#{path}/#{key}")
-          httpreq = Net::HTTP::Get.new("#{path}/#{key}")
-
-          # If we can't connect to etcd at all we throw an exception and
-          # block the run
-          begin
-            httpres = @http.request(httpreq)
-          rescue Exception => e
-            Hiera.warn("[hiera-etcd]: Net::HTTP threw exception #{e.message}")
-            raise Exception, e.message
-            next
-          end
-
-          # If we don't find any results in etcd we continue on to the
-          # next path
-          unless httpres.kind_of?(Net::HTTPSuccess)
-            Hiera.debug("[hiera-etcd]: #{httpres.code} HTTP response from #{@config[:host]}:#{@config[:port]}#{path}/#{key}")
-            next
-          end
-
-          # On to the next path if we don't have a response
-          next unless httpres.body
-          # Parse result from standard etcd JSON response
-          result = JSON.parse(httpres.body)['value']
-          parsed_result = Backend.parse_answer(result, scope)
-
-          # Format response as specified type, either array, hash or text
-          case resolution_type
-          when :array
-            answer ||= []
-            answer << parsed_result
-          when :hash
-            answer ||= {}
-            answer = parsed_result.merge answer
-          else
-            answer = parsed_result
-            break
-          end
+          Hiera.debug("[hiera-etcd]: Lookup #{path}/#{key} on #{@config[:host]}:#{@config[:port]}")
+          result = @client.get("#{path}/#{key}")
+          answer = self.parse_result(result, resolution_type)
+          next unless answer
+          break
         end
         answer
       end
 
+
+      def parse_result(res, type)
+        answer = nil
+        case type
+        when :array
+          answer ||= []
+          begin
+            data = Backend.parse_answer(JSON[res], scope)
+            answer << data
+          rescue
+            Hiera.warn("[hiera-etcd]: '#{res}' is not in json format, and array lookup is requested")
+          end
+        when :hash
+          answer ||= {}
+          begin
+            data = Backend.parse_answer(JSON[res], scope)
+            answer << data
+          rescue
+            Hiera.warn("[hiera-etcd]: '#{res}' is not in json format, and hash lookup is requested")
+          end
+        else
+          answer = Backend.parse_answer(result, scope)
+        end
+        answer
+      end
     end
   end
 end
